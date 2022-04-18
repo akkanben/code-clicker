52d390645488e2f277e65c42b11da64cb0e340b8
Ben Mills <fuzzyllama@gmail.com>
2022-04-18 12:32:51 -0700

    Udates upgrade model to include visibility and remove threshold

--- app/src/main/java/com/crudalchemy/codeclicker/models/Upgrade.java
+++ app/src/main/java/com/crudalchemy/codeclicker/models/Upgrade.java
@@ -5,16 +5,19 @@ public class Upgrade
 {
     Generator generator;
     String name;
+    String image;
     String description;
-    double appearThreshold;
+    int cost;
+    UpgradeType type;
+    boolean isVisible;
 
-
-    public Upgrade(Generator generator, String name, String description, double appearThreshold)
+    public Upgrade(String name, String image, String description, int cost, UpgradeType type)
     {
-        this.generator = generator;
         this.name = name;
+        this.image = image;
+        this.cost = cost;
         this.description = description;
-        this.appearThreshold = appearThreshold;
+        this.type = type;
     }
 
     public void increaseMultiplier()
@@ -39,6 +42,22 @@ public class Upgrade
         this.name = name;
     }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -47,11 +66,19 @@ public class Upgrade
         this.description = description;
     }
 
-    public double getAppearThreshold() {
-        return appearThreshold;
+    public UpgradeType getType() {
+        return type;
     }
 
-    public void setAppearThreshold(double appearThreshold) {
-        this.appearThreshold = appearThreshold;
+    public void setType(UpgradeType type) {
+        this.type = type;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
     }
 }
