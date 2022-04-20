5a25c2903dbd7cbbb21b2681256aed5205b0da9b
Eddward-Regalado <tregalado30@gmail.com>
2022-04-19 20:46:08 -0700

    added layout animations, added popup dialog boxes for generator and upgrade recyclerViews

--- app/src/main/java/com/crudalchemy/codeclicker/activity/MainActivity.java
+++ app/src/main/java/com/crudalchemy/codeclicker/activity/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     GeneratorMenuRecyclerViewAdapter generatorMenuRecyclerViewAdapter;
     UpgradeMenuRecyclerViewAdapter upgradeMenuRecyclerViewAdapter;
 
+    GeneratorMenuRecyclerViewAdapter generatorAdapter;
+    UpgradeMenuRecyclerViewAdapter upgradeAdapter;
+
     String helloWorldCodeStr = "class Greeting{ \n   public static void main(String args[]){";
 
     @Override
@@ -49,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
        }*/
         setUpSaveLoad();
-        setupGeneratorButton();
+//        setupGeneratorButton();
         setupUpgradeItemRecyclerView();
         gameLoop = new GameLoop("game");
         gameLoop.start();
+
+        setupPopupGeneratorButton();
+        setupPopupUpgradesButton();
 
     }
 
@@ -118,42 +125,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void setupGeneratorButton()
-    {
-        Button generatorButton = findViewById(R.id.button_main_activity_generators);
-        Button upgradeButton = findViewById(R.id.button_main_activity_upgrades);
-        Button enterButton = findViewById(R.id.button_main_activity_click);
-        RecyclerView upgradeItemListRecyclerView = (RecyclerView) findViewById(R.id.upgrade_menu_list_recycler_view_upgrades);
-        RecyclerView generatorItemListRecyclerView = (RecyclerView) findViewById(R.id.upgrade_menu_list_recycler_view_generators);
+//    private void setupGeneratorButton()
+//    {
+//        Button generatorButton = findViewById(R.id.button_main_activity_generators);
+//        Button upgradeButton = findViewById(R.id.button_main_activity_upgrades);
+//        Button enterButton = findViewById(R.id.button_main_activity_click);
+//        RecyclerView upgradeItemListRecyclerView = (RecyclerView) findViewById(R.id.upgrade_menu_list_recycler_view_upgrades);
+//        RecyclerView generatorItemListRecyclerView = (RecyclerView) findViewById(R.id.upgrade_menu_list_recycler_view_generators);
+//
+//        generatorButton.setOnClickListener(view ->
+//        {
+//            if (generatorItemListRecyclerView.getVisibility() == View.VISIBLE) {
+//                enterButton.setVisibility(View.VISIBLE);
+//                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
+//                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
+//            }
+//            else {
+//                enterButton.setVisibility(View.INVISIBLE);
+//                generatorItemListRecyclerView.setVisibility(View.VISIBLE);
+//                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
+//            }
+//        });
+//
+//        upgradeButton.setOnClickListener(view -> {
+//            if (upgradeItemListRecyclerView.getVisibility() == View.VISIBLE) {
+//                enterButton.setVisibility(View.VISIBLE);
+//                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
+//                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
+//            }
+//            else {
+//                enterButton.setVisibility(View.INVISIBLE);
+//                upgradeItemListRecyclerView.setVisibility(View.VISIBLE);
+//                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
+//            }
+//        });
+//
+//    }
 
-        generatorButton.setOnClickListener(view ->
-        {
-            if (generatorItemListRecyclerView.getVisibility() == View.VISIBLE) {
-                enterButton.setVisibility(View.VISIBLE);
-                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
-                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
-            }
-            else {
-                enterButton.setVisibility(View.INVISIBLE);
-                generatorItemListRecyclerView.setVisibility(View.VISIBLE);
-                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        upgradeButton.setOnClickListener(view -> {
-            if (upgradeItemListRecyclerView.getVisibility() == View.VISIBLE) {
-                enterButton.setVisibility(View.VISIBLE);
-                upgradeItemListRecyclerView.setVisibility(View.INVISIBLE);
-                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
-            }
-            else {
-                enterButton.setVisibility(View.INVISIBLE);
-                upgradeItemListRecyclerView.setVisibility(View.VISIBLE);
-                generatorItemListRecyclerView.setVisibility(View.INVISIBLE);
-            }
-        });
-
-    }
 
     private void setUpSaveLoad()
     {
@@ -188,6 +196,53 @@ public class MainActivity extends AppCompatActivity {
         generatorItemListRecyclerView.setLayoutManager(generatorLayoutManager);
         generatorMenuRecyclerViewAdapter = new GeneratorMenuRecyclerViewAdapter(game, this);
         generatorItemListRecyclerView.setAdapter(generatorMenuRecyclerViewAdapter);
+
+    }
+
+    public void setupPopupGeneratorButton()
+    {
+        Button button = (Button) findViewById(R.id.button_main_activity_generators);
+        button.setOnClickListener(b ->
+        {
+            showPopupGeneratorDialogBox();
+        });
+    }
+
+    public void setupPopupUpgradesButton()
+    {
+        Button button = (Button) findViewById(R.id.button_main_activity_upgrades);
+        button.setOnClickListener(b ->
+        {
+            showPopupUpgradesDialogBox();
+        });
+    }
+
+    public void showPopupGeneratorDialogBox()
+    {
+
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.popup_generator);
+        RecyclerView rv = (RecyclerView) dialog.findViewById(R.id.popup_generator_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(layoutManager);
+        generatorAdapter = new GeneratorMenuRecyclerViewAdapter(game, this);
+        rv.setAdapter(generatorAdapter);
+        dialog.show();
+    }
+
+    public void showPopupUpgradesDialogBox()
+    {
+
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.popup_upgrades);
+        RecyclerView rv = (RecyclerView) dialog.findViewById(R.id.popup_upgrades_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(layoutManager);
+        upgradeAdapter = new UpgradeMenuRecyclerViewAdapter(game, this);
+        rv.setAdapter(upgradeAdapter);
+        dialog.show();
     }
 
 }
