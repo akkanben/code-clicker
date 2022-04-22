52eb74f5ebc7a4ef47c40299549db5d737e5b32e
Ben Mills <fuzzyllama@gmail.com>
2022-04-21 20:05:24 -0700

    Updates room to overwrite on conflict

--- app/src/main/java/com/crudalchemy/codeclicker/room/CodeClickerDatabase.java
+++ app/src/main/java/com/crudalchemy/codeclicker/room/CodeClickerDatabase.java
@@ -7,7 +7,7 @@ import com.crudalchemy.codeclicker.activity.Game;
 import com.crudalchemy.codeclicker.models.Generator;
 import com.crudalchemy.codeclicker.models.Upgrade;
 
-@Database(entities = {Game.class, Generator.class, Upgrade.class}, version = 4)
+@Database(entities = {Game.class, Generator.class, Upgrade.class}, version = 8)
 public abstract class CodeClickerDatabase extends RoomDatabase
 {
     public abstract CodeClickerDao dao();
